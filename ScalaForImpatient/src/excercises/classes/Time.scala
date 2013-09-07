e24feac48c5f5b99f5a8dceed6f52d43a69ab6df
Sudipta Deb <sudipta.deb@gmail.com>
2013-09-07 21:14:41 +0530

    Refactored the code by removing unnecessary Method call

--- ScalaForImpatient/src/excercises/classes/Time.scala
+++ ScalaForImpatient/src/excercises/classes/Time.scala
@@ -1,21 +1,19 @@
 package excercises.classes
 
-class Time(private var hours : Int = 0, private var minutes : Int = 0) {
-  
-	var m = hours * 60 + minutes
-	def before(other : Time) = {
-	 hours * 60 + minutes < other.hours * 60 + other.minutes
-	}
+class Time(val hours : Int = 0, val minutes : Int = 0) {
+	val convertedTimeInMinutes = hours * 24 + minutes
 	
-	def hourVal = m/60
-	def minuteVal = m%60
+	def before(other : Time) = convertedTimeInMinutes < other.convertedTimeInMinutes 
+	
+	override def toString = "Time is: Hours: " + hours + " Minutes: " + minutes;
 }
 
 object TimeComp{
   def main(args : Array[String]){
     val time = new Time(12,0)
     val other = new Time(12,1)
-    println(time.hourVal + ":" + time.minuteVal)
+    println(time)
+    println(other)
     println(time.before(other))
   }
 }
