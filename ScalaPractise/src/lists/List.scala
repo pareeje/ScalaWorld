28c21585ab326beddb6d52e2ed2266b7f3abfe3f
paromita <paromita.kol@gmail.com>
2013-11-07 21:21:25 +0530

    Lists - initial version

---
+++ ScalaPractise/src/lists/List.scala
@@ -0,0 +1,23 @@
+package lists
+
+trait List[+T] {
+  
+  def isEmpty : Boolean
+  def head : T
+  def tail : List[T]
+  def prepend [U >: T] (elem : U) : List[U] = new Cons(elem,this)
+}
+
+class Cons[T](val head:T, val tail : List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty = true
+  def head = throw new NoSuchElementException("Nil.head")
+  def tail = throw new NoSuchElementException("Nil.tail")
+}
+
+object test {
+  val x : List[String] = Nil
+}
