1c5bcc7550c6cedffca9a7a2b21966dedeac21f6
paromita <paromita.kol@gmail.com>
2013-11-08 15:44:35 +0530

    Added Prod and Var to Expr

--- ScalaPractise/src/expressions/Expr.scala
+++ ScalaPractise/src/expressions/Expr.scala
@@ -11,18 +11,37 @@ trait Expr {
 }
 
 case class Number(n:Int) extends Expr
-case class Sum( e1: Number, e2: Number) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Prod(e1 : Expr, e2 : Expr) extends Expr
+case class Var(x:Char) extends Expr
 
 object testExpr {
   def show(exp : Expr) : String = {
     exp match {
       case Number(x) => x.toString()
-      case Sum(e1:Number, e2:Number) => show(e1) + "+" + show(e2)
+      case Sum(e1, e2) => show(e1) + "+" + show(e2)
+      case Prod(e1,e2) => 
+        val st1 = e1 match {
+          case Sum(a,b) => "(" + show(a) + "+" + show(b) + ")"
+          case _ => show(e1)
+        }
+        val st2 = e2 match {
+          case Sum(a,b) => "(" + show(a) + "+" + show(b) + ")"
+          case _ => show(e2)
+        }
+        st1 + "*" + st2
+      case Var(x) => x.toString
     }
   }
 }
 
 object test extends App{
+  
   println(testExpr.show(Sum(Number(1),Number(2))))
   println(Sum(Number(1),Number(3)).eval)
+  println(testExpr.show(Sum(Number(2),Prod(Number(10),Var('x')))))
+  
+  println(testExpr.show(Sum(Prod(Number(2),Var('x')),Var('y'))))
+  println(testExpr.show(Prod(Sum(Number(2),Var('x')),Var('y'))))
+  println(testExpr.show(Prod(Sum(Number(2),Var('x')),Sum(Number(3),Var('y')))))
 }
