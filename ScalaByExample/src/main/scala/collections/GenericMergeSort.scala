d83083fea4b3a583412ebabacc9291fae155847d
paromita <paromita.kol@gmail.com>
2013-11-24 21:29:06 +0530

    Just use Ordering for all comparisons

--- ScalaByExample/src/main/scala/collections/GenericMergeSort.scala
+++ ScalaByExample/src/main/scala/collections/GenericMergeSort.scala
@@ -1,8 +1,10 @@
 package collections
 
+import scala.math.Ordering
+
 object GenericMergeSort {
 
-  def msort[T]( xs : List[T])(lt : (T,T)=>Boolean) : List[T] = {
+  def msort[T]( xs : List[T])(ord : Ordering[T]) : List[T] = {
     val n = xs.length/2
     if( n == 0) xs // only 1 or 0 elements 
     else {
@@ -11,12 +13,12 @@ object GenericMergeSort {
           case (xs, Nil) => xs
           case (Nil, ys) => ys
           case(x::xs1,y::ys1) =>
-            if(lt(x,y)) x :: merge(xs1,ys)
+            if(ord.lt(x, y)) x :: merge(xs1,ys)
             else y :: merge(xs,ys1)
         }
       }
       val (fst,snd) = xs splitAt n
-      merge(msort(fst)(lt),msort(snd)(lt))
+      merge(msort(fst)(ord),msort(snd)(ord))
     }
   }
 }
--- ScalaByExample/src/test/scala/collections/GenericMergeSortTest.scala
+++ ScalaByExample/src/test/scala/collections/GenericMergeSortTest.scala
@@ -17,17 +17,17 @@ class GenericMergeSortTest extends FunSuite {
   }
 
   test("Test with multi item List") {
-    val sList = GenericMergeSort.msort(List(1, -4, 8, 2, 3, 0))((x: Int, y: Int) => x < y)
+    val sList = GenericMergeSort.msort(List(1, -4, 8, 2, 3, 0))(Ordering.Int)
     assert(sList.head < 0, "Now first element is negative")
   }
 
   test("Test with boolean items List") {
-    val sList = GenericMergeSort.msort(List(true, false, false, true))((x: Boolean, y: Boolean) => x < y)
+    val sList = GenericMergeSort.msort(List(true, false, false, true))(Ordering.Boolean)
     //assert(sList.head, "Now first element is negative")
     println(sList)
   }
   test("Test with string items List") {
-    val sList = GenericMergeSort.msort(List("xxx", "aaa", "fff", "ddd", "zzz"))((x: String, y: String) => x.compareTo(y) < 0)
+    val sList = GenericMergeSort.msort(List("xxx", "aaa", "fff", "ddd", "zzz"))(Ordering.String)
     println(sList)
   }
 }
